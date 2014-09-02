b19b01031ed64ee55d7cb8f08f5336999428d737
shengxun.lsx <shengxun.lsx@rt1c06035.tbc>
2014-09-02 10:54:02 +0800

    liantong

--- middleware/serve.cpp
+++ middleware/serve.cpp
@@ -18,6 +18,19 @@ inline void oops(string str)         //出错
     exit(EXIT_FAILURE);
 }
 
+void init()
+{
+    sockaddr_in servaddr;
+	bzero(&servaddr, sizeof(servaddr)); 
+	servaddr.sin_family = AF_INET;                    //协议设置
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);     //绑定本地
+	servaddr.sin_port = htons(SERV_PORT); 
+	if(bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)         //本地结构体用于bind
+			oops("bind error");  
+
+    
+}
+
 void do_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen) 
 { 
 	int n; 
@@ -34,17 +47,10 @@ void do_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen)
 } 
 int main(void) 
 { 
-	int sockfd; 
-	struct sockaddr_in servaddr, cliaddr; 
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
-	bzero(&servaddr, sizeof(servaddr)); 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	servaddr.sin_port = htons(SERV_PORT); 
-	if(bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) 
-	{ 
-			oops("bind error");  
-	} 
+	int sockfd=socket(AF_INET, SOCK_DGRAM, 0); 
+	
+
+	sockaddr_in client；
 	cout<<"service start:"<<endl;
 	do_echo(sockfd, (struct sockaddr *)&cliaddr, sizeof(cliaddr)); 
 	return 0; 
